22147da19cb1735ab86ae459a2d5629f7ffc37ed
Angelia <rose20.99.c@gmail.com>
2022-04-06 16:25:55 +0800

    Update sehuatang.js

--- sehuatang/sehuatang.js
+++ sehuatang/sehuatang.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name           H28
+// @name         sehuatang
 // @namespace    https://www.sehuatang.net/forum-103-1.html
 // @description  直接把帖子列表转成图片浏览,点击图片可以进入帖子.
 // @author       123
@@ -9,41 +9,51 @@
 // @require https://code.jquery.com/jquery-3.4.1.min.js
 // @version 3
 // ==/UserScript==
-$(document).ready(function () {
-  $('.icn').each(function () {
-    var urls = "https://www.sehuatang.net/";
-    urls += $(this).find("a").attr("href");
-    var imgg = $(this).find("img");
-    var icn_td = $(this);
+$(document).ready(function(){
+$('.icn').each(function(){
+    var urls="https://www.sehuatang.net/";
+    urls+=$(this).find("a").attr("href");
+    var imgg= $(this).find("img");
+    var icn_td=$(this);
+    console.log(urls);
+
+    var href = document.location.href;
+
     GM_xmlhttpRequest({
-      method: 'GET',
-      url: urls,
-      headers: {
-        'User-agent': 'Mozilla/5.0  Chrome/70. Safari/537.36',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
-      },
-      onload: function (result) {
-        var doc = result.responseText;
-        var img01 = $(doc).find(".zoom").attr("file");
-        $(imgg).attr("src", img01);
-        var dvi01 = $(doc).find(".blockcode");
-        var dvi02 = $(doc).find("#thread_subject");
-        //console.log($(dvi02).html());
-        var magnet = $(dvi01).find("li").text();
-        //console.log(magnet);
-        $(icn_td).css("width", "100%")
-        $(icn_td).append("<p style='margin-top: 20px;font-size: 2em;'>" + $(dvi02).html() + "</p><br/>");
-        $(icn_td).append("<p style='margin-top: 20px;font-size: 2em;'>" + magnet + "</p><br/>");
-      }
+        method: 'GET',
+        url: urls,
+        headers: {
+            'User-agent': window.navigator.userAgent,
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'cookie': document.cookie,
+            'referer': href,
+        },
+        onerror:function(e){
+          console.log(e);
+        },
+        onload: function(result) {
+            var doc = result.responseText;
+            console.log(doc);
+            var img01=$(doc).find(".zoom").attr("file");
+            $(imgg).attr("src",img01);
+            var dvi01=$(doc).find(".blockcode");
+            var dvi02=$(doc).find("#thread_subject");
+            //console.log($(dvi02).html());
+            var magnet =$(dvi01).find("li").text();
+            //console.log(magnet);
+            $(icn_td).css("width","100%")
+            $(icn_td).append("<p style='margin-top: 20px;font-size: 2em;'>"+$(dvi02).html()+"</p><br/>");
+            $(icn_td).append("<p style='margin-top: 20px;font-size: 2em;'>"+magnet+"</p><br/>");
+        }
     });
-  });
-  $('.common').each(function () {
+});
+$('.common').each(function(){
     $(this).remove();
-  });
-  $('.by').each(function () {
+});
+$('.by').each(function(){
     $(this).remove();
-  });
-  $('.num').each(function () {
+});
+$('.num').each(function(){
     $(this).remove();
-  });
+});
 });
